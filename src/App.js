a8e611c26a1afe04270948384e3d0dfd1f1fd113
Renaud Genevois <KQHLZ2@ad.insee.intra>
2020-08-28 09:32:16 +0200

    create

--- src/App.js
+++ src/App.js
@@ -1,15 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import { createStore, Suggester, StoreManager } from "idb-suggester";
+import "./App.scss";
+import "./custom-option.scss";
+
+const COG_IDB_NAME = "TEST/COG";
+const COG_FIELDS = [{ name: "libelle" }, { name: "com" }, { name: "nccenr" }];
+
+function CustomCOGOption({ suggestion }) {
+  const { com, libelle } = suggestion;
+  return (
+    <div className="custom-cog-option">
+      <span className="com">{com}</span>
+      <span className="libelle">{libelle}</span>
+    </div>
+  );
+}
+
+async function fetchCommunes() {
+  const communes = await fetch("/communes-2019.json").then((data) =>
+    data.json()
+  );
+  return communes.map(function (commune, i) {
+    const { com } = commune;
+    return { ...commune, id: `COM-${i}-${com}` };
+  });
+}
 
 function App() {
+  const [store, setStore] = useState(undefined);
+  useEffect(function () {
+    async function init() {
+      setStore(await createStore(COG_IDB_NAME, 1, COG_FIELDS));
+    }
+
+    init();
+  }, []);
+  if (!store) {
+    return null;
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div style={{ width: "250px" }}>
+          <StoreManager
+            name={COG_IDB_NAME}
+            version={1}
+            fields={COG_FIELDS}
+            fetch={fetchCommunes}
+          />
+          <Suggester
+            store={store}
+            placeHolder="Rechercher dans le COG."
+            optionComponent={CustomCOGOption}
+            displayPath="libelle"
+            onSelect={function (item) {
+              console.log("onSelect", item);
+            }}
+          />
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
