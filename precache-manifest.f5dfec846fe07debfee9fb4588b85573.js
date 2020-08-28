28e5c5de7417804f8d38ef08db329252c405f93f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-28 08:08:33 +0000

    Deploy renaud23/test-idb-suggester to github.com/renaud23/test-idb-suggester.git:gh-pages

---
+++ precache-manifest.f5dfec846fe07debfee9fb4588b85573.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c187c1e45f6ae64b878532c3c37e77e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7164667227aa0f10e64f",
+    "url": "/static/css/main.1b7b9f6f.chunk.css"
+  },
+  {
+    "revision": "c06699e7fab89dcb24ba",
+    "url": "/static/js/2.4959968d.chunk.js"
+  },
+  {
+    "revision": "582ab84fe1aa22c1e690411e604a62db",
+    "url": "/static/js/2.4959968d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7164667227aa0f10e64f",
+    "url": "/static/js/main.a768ca3b.chunk.js"
+  },
+  {
+    "revision": "93966c3367b14851fd48",
+    "url": "/static/js/runtime-main.9bf21d43.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
